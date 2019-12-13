e3b2fb94b8c994648006039e32fa72a47256f2a3
Filip <filip.lukasz.lukaszewski@gmail.com>
2019-12-13 12:59:35 +0100

    Added gitignore and changed main.py structure

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import os
 
-dir = "/dev/sdb"
+
+dir = "/dev/disk2"
 ROOT_DIR = os.path.abspath(os.curdir)
 iso = "Fedora-Workstation-Live-x86_64-31-1.9.iso"
-
 iso_dir = ROOT_DIR + "/" + iso
 
-command = 'dd if={} of={} bs=2048'.format(iso_dir, dir)
+
+def flash_iso(iso_dir, disk_dir):
+    command = 'dd if={} of={} bs=4096'.format(iso_dir, disk_dir)
+    # os.system(command)
+    print("I have printed the command! \n {}").format(command)
 
 
-os.system(command)
 
 
